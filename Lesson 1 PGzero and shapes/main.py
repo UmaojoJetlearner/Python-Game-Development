b800923741220b53be6157ec992b91431925ff6e
UmaojoJetlearner <umaojo.okpanachi@gmail.com>
2026-02-08 13:55:07 -0600

    Add files via upload

--- Lesson 1 PGzero and shapes/main.py
+++ Lesson 1 PGzero and shapes/main.py
@@ -1,14 +1,26 @@
 import pgzrun
+import random
+
+
 
 WIDTH=300
 HEIGHT=300
 
-width=20
-height=30
-def draw():
-    rect = Rect((0,0),(width,height))
-    screen.draw.rect(rect,("blue"))
+width=WIDTH
+height=HEIGHT-200
 
+r=random.randint(0,255)
+g=random.randint(122,255)
+b=0
+def draw():
+    global width,height,b
+    for i in range(20):
+        rect = Rect((0,0),(width,height))
+        rect.center=(150,150)
+        screen.draw.rect(rect,(r,g,b))
+        width-=10
+        height+=10
+        
 def update():
     pass
 

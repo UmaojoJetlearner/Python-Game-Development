338341c5dfc0ba26f8f231d6a36b9a544bdfe316
UmaojoJetlearner <umaojo.okpanachi@gmail.com>
2026-02-22 13:58:05 -0600

    Add files via upload

---
+++ 2D list/Main.py
@@ -0,0 +1,15 @@
+#2d list
+
+my_list=["food","water","sodas",[3,5]]
+print(my_list)
+print(my_list[3][1])
+
+
+dlist=[
+    [1,2,3],
+    [6,6,7],
+    [5,8,5]
+]
+print(dlist)
+print(dlist[0][0])
+print(dlist[1][2])

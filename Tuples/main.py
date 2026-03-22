3897ad616f87d24646170b1a2ab3e79c3b2f089b
UmaojoJetlearner <umaojo.okpanachi@gmail.com>
2026-03-22 13:06:37 -0500

    Add files via upload

---
+++ Tuples/main.py
@@ -0,0 +1,36 @@
+# Packing
+# my_tuple=("football","xbox controller","my laptop","coding")
+
+# Unpacking
+# sport,gaming,technology,skill=my_tuple
+
+# code
+# print(sport)
+# print(skill)
+
+Umaojo_reportcard=("Umaojo",95,92,98)
+Name,Math,English,Science=Umaojo_reportcard
+
+print(Name)
+print(Math)
+print(English)
+print(Science)
+
+Alex_reportcard=("Alex",89,94,97)
+Name,Math,English,Science=Alex_reportcard
+
+print(Name)
+print(Math)
+print(English)
+print(Science)
+
+Ari_reportcard=("Ari",87,96,93)
+Name,Math,English,Science=Ari_reportcard
+
+print(Name)
+print(Math)
+print(English)
+print(Science)
+
+
+Reportcard_list=[Ari_reportcard,Alex_reportcard,Umaojo_reportcard]

570c74497f24c8e6ae112182959259648a1e4a3b
UmaojoJetlearner <umaojo.okpanachi@gmail.com>
2026-04-12 13:27:54 -0500

    Add files via upload

--- Sets/secretsetgame.py
+++ Sets/secretsetgame.py
@@ -9,3 +9,18 @@ guessedset=set()
 print(" Welcome to guess the scret set game, enjoy! \n Guess the number from 1 through 20") 
 while guessedset != secretset:
     guess=int(input("Enter your guess!"))
+
+    if guess in guessedset:
+        print("You already guessed this number")
+        continue
+
+    if guess in secretset:
+        print("Correct guess :)")
+        guessedset.add(guess)
+    else:
+        print("Incorrect guess :(")
+    
+    print("Your correct guesses so far",guessedset)
+
+print("Congratulations, you have guessed all the numbers!")
+print("The secret set was",secretset)
